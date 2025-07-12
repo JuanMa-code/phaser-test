d72fc5668f165f9c8be80ef43600d702bcf92c03
Juanma torres <juanma.trrs@gmail.com>
2025-07-12 21:10:40 +0200

    testing changes vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,9 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-   base: '/phaser-test/',
+  base: '/phaser-test/',
   plugins: [react()],
-  root: './',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './src/main.tsx',
-    },
   },
 });
